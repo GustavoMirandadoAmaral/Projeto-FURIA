0b177a09cc2c4570142feef0ca58d43f15712420
Gustavo Miranda do Amaral <gustavomirandadoamaral@gmail.com>
2025-04-25 01:13:36 -0300

    Tela inicial de Bem-Vindo

---
+++ src/components/BemVindo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "../styles/Style.css";
+import simboloFuria from '../assets/images/Furia-Esports.png';
+
+const BemVindo = ({ onStart }) => {
+  return (
+    <motion.div
+      className="bem-vindo-container"
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -30 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img className="logo-furia" src={simboloFuria} alt="Logo" />
+      <h1>
+        Bem-vindo(a) ao <span className="furia-text">FURIA Fan Quiz</span>!
+      </h1>
+      <p>
+        Descubra que tipo de fã da FURIA você é!!{" "}
+        <span role="img" aria-label="fogo">🔥</span>
+      </p>
+      <button onClick={onStart} className="start-button">Começar Quiz</button>
+    </motion.div>
+  );
+};
+
+export default BemVindo;
