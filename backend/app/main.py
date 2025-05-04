19a1efc465998ae9c7953e292b40162773db5b77
Gustavo Miranda do Amaral <gustavomirandadoamaral@gmail.com>
2025-05-04 20:11:53 -0300

    mudanças na main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import create_db
-from app.routers import users, documents, socials, openai_recommendation  # Inclui o novo módulo
+from app.routers import users, documents, socials, openai_recommendation 
 
 app = FastAPI()
 
-# Incluir os roteadores existentes
 app.include_router(users.router)
 app.include_router(documents.router)
 app.include_router(socials.router)
-app.include_router(openai_recommendation.router)  # Roteador da IA
+app.include_router(openai_recommendation.router) 
 
-# Configuração do CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://localhost:3001"],
@@ -20,7 +18,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Evento de inicialização
 @app.on_event("startup")
 def on_startup():
     create_db()
